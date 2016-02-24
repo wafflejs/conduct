3d88ccba07699b87d3b70e7fb0713bb847c006f8
Visnu Pitiyanuvath <visnu@opendoor.com>
2016-02-23 19:09:13 -0800

    incident posting

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app = express()
 app.use(require('body-parser').urlencoded({ extended: false }))
 
 app.use(function (req, res) {
-  console.log(req.body)
+  request.post({
+    uri: process.env.SLACK_URL,
+    json: {
+      username: 'Incident',
+      icon_emoji: ':japanese_ogre:',
+      text: req.body.Body
+    }
+  })
   res.status(204).end()
 })
 
