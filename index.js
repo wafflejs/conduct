cf34149854dc1f937fe11cf0a1ccdac91e0cd796
Visnu Pitiyanuvath <visnu@opendoor.com>
2016-02-24 15:11:26 -0800

    allow cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const app = express()
 
+app.use(require('cors')())
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: false }))
 
