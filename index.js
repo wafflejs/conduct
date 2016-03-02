848af4de35c0ab45da2ee1eba36e41d10faf1817
Visnu Pitiyanuvath <visnup@gmail.com>
2016-03-01 18:21:17 -0800

    message is more important

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ app.use(require('cors')())
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: false }))
 
-const mentions = '<@visnup> <@billyroh> <@nexxy> <@kellyk> <@thealphanerd>: '
+const mentions = ' <@visnup> <@billyroh> <@nexxy> <@kellyk> <@thealphanerd>'
 app.use(function (req, res) {
   request.post({
     uri: process.env.SLACK_URL,
     json: {
       username: 'Incident',
       icon_emoji: ':japanese_ogre:',
-      text: mentions + (req.body.Body || req.body.body || req.body.description)
+      text: (req.body.Body || req.body.body || req.body.description) + mentions
     }
   })
   res.status(204).end()
