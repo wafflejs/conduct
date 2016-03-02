62386016127e8652c67d52745b03e84266ebb51e
Visnu Pitiyanuvath <visnup@gmail.com>
2016-03-01 18:02:26 -0800

    more direct mentioning

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ app.use(require('cors')())
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: false }))
 
+const mentions = '<@visnup> <@billyroh> <@nexxy> <@kellyk> <@thealphanerd>: '
 app.use(function (req, res) {
-  var text = req.body.Body || req.body.body || req.body.description
-  text = '<@here>: ' + text
-
   request.post({
     uri: process.env.SLACK_URL,
     json: {
       username: 'Incident',
       icon_emoji: ':japanese_ogre:',
-      text: text
+      text: mentions + (req.body.Body || req.body.body || req.body.description)
     }
   })
   res.status(204).end()
