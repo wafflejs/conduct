49de1883d8d91582345fdbb763b0cebf5bd06d59
Visnu Pitiyanuvath <visnup@gmail.com>
2016-03-01 17:50:15 -0800

    try to @here

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: false }))
 
 app.use(function (req, res) {
+  var text = req.body.Body || req.body.body || req.body.description
+  text = '<@here>: ' + text
+
   request.post({
     uri: process.env.SLACK_URL,
     json: {
       username: 'Incident',
       icon_emoji: ':japanese_ogre:',
-      text: req.body.Body || req.body.body || req.body.description
+      text: text
     }
   })
   res.status(204).end()
